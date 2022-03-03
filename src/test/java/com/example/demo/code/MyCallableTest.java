5292939cbe09c685d3164115fc93af1744169231
tntis <tnbinsu@gmail.com>
2022-03-03 22:36:47 +0900

    12번째 study

--- src/test/java/com/example/demo/code/MyCallableTest.java
+++ src/test/java/com/example/demo/code/MyCallableTest.java
@@ -2,6 +2,10 @@ package com.example.demo.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.IntBinaryOperator;
 import java.util.function.Supplier;
 
@@ -65,6 +69,75 @@ class MyCallableTest {
         System.out.println(">>>this 2= " + this);
         System.out.println("callable = " + callable);
     }
+
+    @Test
+    void runnable() {
+        Runnable runnable = () -> System.out.println("running...");
+        runnable.run();
+    }
+
+    @Test
+    void supplier() {
+        Supplier<String> supplier = () -> "supplier";
+        String s = supplier.get();
+        System.out.println("s = " + s);
+    }
+
+    @Test
+    void consumer() {
+        //Generic은 Wrapper 타입 또는 Rㄷference 타입만 가능
+        Consumer<Integer> consumer1 = i -> System.out.println("integer1 = " + i);
+        Consumer<Integer> consumer2 = i -> System.out.println("integer2 = " + i);
+
+
+        consumer1.accept(9999);
+        consumer2.accept(5555);
+
+        //함수형 프로그래밍의 스타일.. Stream, optional, Webflux(비동기)
+        consumer1.andThen(consumer2).accept(8888);
+        consumer1.andThen(consumer2).accept(8888);
+
+        // 종단(Terminated) 메소드
+    }
+
+    @Test
+    void function() {
+        Function<Integer, Boolean> function = i -> i > 0;
+        Boolean result1 = function.apply(0);
+        Boolean result2 = function.apply(23);
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+    }
+
+    @Test
+    void function2() {
+        Function<Integer, Integer> multiply = (value) -> value * 2;
+        Function<Integer, Integer> add      = (value) -> value + 3;
+
+        System.out.println("multiply.apply(3) = " + multiply.apply(3));
+        System.out.println("add.apply(5) = " + add.apply(5));
+
+        System.out.println("multiply.andThen(add).apply(4) = " + multiply.andThen(add).apply(4));   // 4*2 +3
+        System.out.println("multiply.compose(add).apply(4) = " + multiply.compose(add).apply(4));  // 4+3 *2
+    }
+
+    @Test
+    void optional() {
+        // 정적 팩토리 메소드 패턴(디자인 패턴)
+        String s = null;
+      //  Optional<String> optS1 = Optional.of(s); // null 이면 에러가 남
+        Optional<String> optS2 = Optional.ofNullable(s); // null 일수 도 있음 에러발생 ㄴ
+
+        String s1 = optS2.orElse("Default");  // null 아니면 값을 표출, null 이면 orElse에 있는 값을 보여줌
+        System.out.println("s1 = " + s1);
+        
+        Optional<Object> optS3 = Optional.empty();   // 만들어는졌지만 아무것도 담기지 않음
+
+
+    }
+
+
+
 }
 
 // @ 객체 , $ 클래스
